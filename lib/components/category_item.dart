51cec1d123d35f898cd9e703ccd04b1b6fe8ec86
João Victor Terzi <jv_ermel@hotmail.com>
2022-05-31 15:06:15 -0300

    Creating a CategoryItem Widget

---
+++ lib/components/category_item.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({Key? key, required this.category}) : super(key: key);
+  
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(category.title);
+  }
+}
