8b4f50518f05c5df12d5c1aabc466d4ea480be05
João Victor Terzi <jv_ermel@hotmail.com>
2022-05-31 15:28:10 -0300

    Styling the categories

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -3,11 +3,27 @@ import '../models/category.dart';
 
 class CategoryItem extends StatelessWidget {
   const CategoryItem({Key? key, required this.category}) : super(key: key);
-  
+
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return Text(category.title);
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(15)),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+      ),
+    );
   }
 }
