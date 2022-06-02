390a922766a0bf781d1aa30a800e8f9c287a1d95
João Victor Terzi <jv_ermel@hotmail.com>
2022-06-02 16:13:16 -0300

    Moving the tab bar for the bottom of the app

--- lib/pages/tabs_page.dart
+++ lib/pages/tabs_page.dart
@@ -2,35 +2,56 @@ import 'package:flutter/material.dart';
 import 'package:meals/pages/categories_page.dart';
 import 'package:meals/pages/favorites_page.dart';
 
-class TabsPage extends StatelessWidget {
+class TabsPage extends StatefulWidget {
   const TabsPage({Key? key}) : super(key: key);
 
   @override
+  State<TabsPage> createState() => _TabsPageState();
+}
+
+class _TabsPageState extends State<TabsPage> {
+  int _selectedPageIndex = 0;
+
+  final List<String> _titles = [
+    'Lista de Categorias',
+    'Meus Favoritos',
+  ];
+
+  final List<Widget> _screens = [
+    const CategoriesPage(),
+    const FavoritesPage(),
+  ];
+
+  _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Vamos cozinhar?'),
-          bottom: const TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_titles[_selectedPageIndex]),
+        centerTitle: true,
+      ),
+      body: _screens[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).colorScheme.secondary,
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categorias',
           ),
-        ),
-        body: const TabBarView(
-          children: [
-            CategoriesPage(),
-            FavoritesPage(),
-          ],
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favoritos',
+          ),
+        ],
       ),
     );
   }
