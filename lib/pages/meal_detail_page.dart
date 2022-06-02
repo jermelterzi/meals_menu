c8bb766ae349f478c37dc421bd8255f8eb41d94b
João Victor Terzi <jv_ermel@hotmail.com>
2022-06-02 15:05:25 -0300

    Finishing de meal detail page

--- lib/pages/meal_detail_page.dart
+++ lib/pages/meal_detail_page.dart
@@ -9,11 +9,66 @@ class MealDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final meal = ModalRoute.of(context)?.settings.arguments as Meal;
 
+    Widget mealTitle(String title) {
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+      );
+    }
+
+    Widget mealSection(Widget child) {
+      return Container(
+        height: MediaQuery.of(context).size.height * 0.4,
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.all(8),
+        child: child,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Text(meal.id),
-      );
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.width,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            mealTitle('Ingredientes'),
+            mealSection(
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    title: Text(meal.ingredients[index]),
+                  );
+                },
+              ),
+            ),
+            mealTitle('Passos'),
+            mealSection(
+              ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    title: Text('${index + 1}. ${meal.steps[index]}'),
+                  );
+                },
+                itemCount: meal.steps.length,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
